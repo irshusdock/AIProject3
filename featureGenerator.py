34e8b95982b4c997a21c851d543d8ca5af5a75b6
Ian Shusdock <ishusdock@gmail.com>
2016-02-07 18:30:46 -0500

    UNTESTED: Added features 3,4,5

--- featureGenerator.py
+++ featureGenerator.py
@@ -12,13 +12,15 @@ def generate_feature_1(line):
 "line is the board to use to generate the feature"
 "This feature is which player has more pieces in the center three columns"
 def generate_feature_2(line):
+	line = line.replace(",", "")
+	
 	board_sum = 0
 
 	"Add the number of player1s pieces in the central 3 rows and subtract the number of player2s pieces"
 	for x in range(12,30):
-		if(line[x] == 1):
+		if(line[x] == "1"):
 			board_sum = board_sum + 1
-		if(line[x] == 2):
+		if(line[x] == "2"):
 			board_sum = board_sum - 1
 
 	"Positive board_sum means player 1 has more pieces, negative means player2, 0 means same number"
@@ -28,14 +30,109 @@ def generate_feature_2(line):
 		return 2
 	return 0
 
+"Generate the value for the third feature"
+"line is the board to use to generate the feature"
+"This feature is the number of columns that player 1 has at least one piece in"
 def generate_feature_3(line):
-	return 0
+	
+	line = line.replace(",", "")
 
+	num_cols = 0;
+
+	for i in range(0,7):
+		player_1_piece = False
+		for j in range(0,6):
+			if(line[(i*6+j)] == "1"):
+				player_1_piece = True
+		if(player_1_piece == True):
+			num_cols = num_cols + 1
+
+	return num_cols
+
+"Generate the value for the third feature"
+"line is the board to use to generate the feature"
+"This feature is the total number of spaces surronding player1s pieces"
+"Open spaces shared by 2 or more pieces are counted multiple times"
 def generate_feature_4(line):
-	return 0
 
+	line = line.replace(",", "")
+	
+	sum_open_spaces = 0
+
+	for i in range(0,7):
+		for j in range(0,6)
+
+			"Ignore any pieces that aren't player1s"
+			if(line[i*6+j] != "1"):
+				continue
+
+			ignore_left = (i==0)
+			ignore_right = (i==6)
+			ignore_up = (j==5)
+			ignore_down = (j==0)
+
+			"Check all spaces to the left"
+			if(not ignore_left):
+				if(line[(i-1)*6+1] == "0"):
+					sum_open_spaces = sum_open_spaces + 1
+				if(not ignore_up):
+					if(line[(i-1)*6+2] == "0"):
+						sum_open_spaces = sum_open_spaces + 1
+				if(not ignore_down):
+					if(line[(i-1)*6+0] == "0"):
+						sum_open_spaces = sum_open_spaces + 1
+
+			"Check all spaces to the right"
+			if(not ignore_right):
+				if(line[(i+1)*6+1] == "0"):
+					sum_open_spaces = sum_open_spaces + 1
+				if(not ignore_up):
+					if(line[(i+1)*6+2] == "0"):
+						sum_open_spaces = sum_open_spaces + 1
+				if(not ignore_down):
+					if(line[(i+1)*6+0] == "0"):
+						sum_open_spaces = sum_open_spaces + 1
+
+			"Check the space directly above"
+			if(not ignore_up):
+				if(line[(i)*6+2] == "0"):
+					sum_open_spaces = sum_open_spaces + 1
+
+			"Check the space directly below"
+			if(not ignore_up):
+				if(line[(i)*6+0] == "0"):
+					sum_open_spaces = sum_open_spaces + 1
+
+	return sum_open_spaces
+
+"Generate the value for the third feature"
+"line is the board to use to generate the feature"
+"This feature is the number of pieces player 1 has along the edge of the board"
 def generate_feature_5(line):
-	return 0
+	line = line.replace(",", "")
+
+	sum_edge_pieces = 0
+
+	"Count the pieces on the left edge of the board, including corners"
+	for x in range(0,6):
+		if(line[x] == "1"):
+			sum_edge_pieces = sum_edge_pieces + 1
+
+	"Count the pieces on the right edge of the board, including corners"
+	for x in range(36,42):
+		if(line[x] == "1"):
+			sum_edge_pieces = sum_edge_pieces + 1
+
+	"Count the pieces on the top edge of the board, not including corners"
+	for x in range(1,6):
+		if(line[x*6+5] == "1"):
+			sum_edge_pieces = sum_edge_pieces + 1
+	"Count the pieces on the bottom edge of the board, not including corners"
+	for x in range(1,6):
+		if(line[x*6] == "1"):
+			sum_edge_pieces = sum_edge_pieces + 1
+
+	return sum_edge_pieces
 
 "Main script for the program"
 def feature_generator_main():
